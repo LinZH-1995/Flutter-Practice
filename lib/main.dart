ca7b73e1c95ddfd0ad2ad6263dd0d2926c9585df
LINZH\lin84 <lin84610@gmail.com>
2025-06-24 22:58:40 +0800

    新增主頁、日歷頁、最愛頁並設定頁面切換邏輯

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/pages/main_page.dart';
+import 'package:flutter_application/pages/calendar_page.dart';
+import 'package:flutter_application/pages/favorite_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,21 +16,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: '今日照片'),
@@ -38,15 +26,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -54,7 +33,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _selectedIndex = 1;
+  int _selectedIndex = 0;
+
+  final List<Widget> _pageList = [
+    const CalendarPage(),
+    const MainPage(),
+    const FavoritePage(),
+  ];
 
   void _onItemTapped(int index) {
     setState(() {
@@ -64,79 +49,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    double deviceWidth = MediaQuery.of(context).size.width;
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Center(child: Text(widget.title)),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Stack(
-              children: [
-                Container(
-                  constraints: BoxConstraints(minHeight: deviceWidth),
-                  width: deviceWidth,
-                  child: Image.network(
-                    'https://apod.nasa.gov/apod/image/2209/WaterlessEarth2_woodshole_2520.jpg',
-                    loadingBuilder:
-                        (
-                          BuildContext context,
-                          Widget child,
-                          ImageChunkEvent? loadingProgress,
-                        ) {
-                          if (loadingProgress == null) return child;
-                          return Center(
-                            child: CircularProgressIndicator(
-                              value: loadingProgress.expectedTotalBytes != null
-                                  ? loadingProgress.cumulativeBytesLoaded /
-                                        loadingProgress.expectedTotalBytes!
-                                  : null,
-                            ),
-                          );
-                        },
-                  ),
-                ),
-                Positioned(
-                  top: 10.0,
-                  right: 10.0,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      print('123');
-                    },
-                    child: const Text('加入最愛'),
-                  ),
-                ),
-              ],
-            ),
-            const Text(
-              '''How much of planet Earth is made of water? Very little, actually.
-            Although oceans of water cover about 70 percent of Earth's surface, these oceans are
-            shallow compared to the Earth's radius. The featured illustration shows what would happen
-             if all of the water on or near the surface of the Earth were bunched up into a ball.
-             The radius of this ball would be only about 700 kilometers, less than half the radius of
-             the Earth's Moon, but slightly larger than Saturn's moon Rhea which, like many moons in
-             our outer Solar System, is mostly water ice. The next smallest ball depicts all of
-             Earth's liquid fresh water, while the tiniest ball shows the volume of all of Earth's
-             fresh-water lakes and rivers. How any of this water came to be on the Earth and whether
-             any significant amount is trapped far beneath Earth's surface remain topics of research.''',
-              style: TextStyle(fontSize: 12, color: Colors.blueGrey),
-            ),
-          ],
-        ),
-      ),
+      body: _pageList[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
