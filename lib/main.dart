44bdff5e6a2246fbcd79d32b903b78127b38310d
LinZH-1995 <125657224+LinZH-1995@users.noreply.github.com>
2025-06-26 13:04:49 +0800

    安裝環境變數套件並設定使用

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_application/pages/main_page.dart';
 import 'package:flutter_application/pages/calendar_page.dart';
 import 'package:flutter_application/pages/favorite_page.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  await dotenv.load(fileName: '.env');
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
